6ba48ea920e63e46c78f7a9a6f1ab6d31cf36da7
Евгений Климов <eklimov84@gmail.com>
2025-10-08 07:55:54 +0300

    feat(TaskSidebar): Pass project to TaskSidebar component
    The TaskSidebar component now accepts a `project` prop. This change is necessary to enable future functionality that requires access to project-level information within the task sidebar.

--- src/components/TaskSidebar.tsx
+++ src/components/TaskSidebar.tsx
@@ -1,6 +1,6 @@
 // src/components/TaskSidebar.tsx
 import React, { useState } from 'react';
-import { PlanItem, Event } from '../types';
+import { PlanItem, Event, Project } from '../types';
 import SubTaskItem from './SubTaskItem';
 import TaskAiActions from './TaskAiActions';
 import ReactMarkdown from 'react-markdown';
@@ -10,13 +10,14 @@ import { FaPlus, FaChevronDown, FaChevronUp } from 'react-icons/fa';
 interface TaskSidebarProps {
   task: PlanItem;
   events: Event[];
+  project: Project;
   isAuditor: boolean;
   isGuest: boolean;
   onAddSubTask: () => void;
   onNewAiEvent: (event: Partial<Event>) => void;
 }
 
-const TaskSidebar: React.FC<TaskSidebarProps> = ({ task, events, isAuditor, isGuest, onAddSubTask, onNewAiEvent }) => {
+const TaskSidebar: React.FC<TaskSidebarProps> = ({ task, events, project, isAuditor, isGuest, onAddSubTask, onNewAiEvent }) => {
   const [isDescriptionExpanded, setIsDescriptionExpanded] = useState(false);
   const descriptionNeedsTruncation = (task.description?.length || 0) > 200;
 
